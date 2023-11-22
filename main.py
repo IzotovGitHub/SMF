dc8e8b0edec6a20e929ae18295f1dbbf25d8a6eb
izotov_a <aleksandr.izotov2020@gmail.com>
2023-11-22 23:24:38 +0300

    SMF-4 Реализовал работу с настройками

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
 import datetime
 import math
-import time
 import warnings
-from Settings import settings as properties
-from Settings import need_show_sifted_figures
-from Settings import print_forecast_out
-from Settings import shift_percent
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -13,65 +8,89 @@ import pandas as pd
 from pandas.errors import SettingWithCopyWarning
 from sklearn.model_selection import train_test_split
 
-
+import Settings
 import util
+from Settings import directory
+from Settings import need_show_sifted_figures
+from Settings import print_forecast_out
+from Settings import random_state
+from Settings import save_to_excel
+from Settings import settings as properties
+from Settings import shift_percent
+from Settings import test_size
+from Settings import to_process
 
-# ================== Подготовка данных ==================
 warnings.simplefilter(action="ignore", category=SettingWithCopyWarning)
 
 train_data = pd.read_excel('Data.xlsx', header=0, index_col='Date', parse_dates=True)
 columns = train_data.columns.values
 
-train_data[columns].plot(figsize=(16, 8), subplots=True)
-plt.xlabel('Дата')
-plt.show()
+if Settings.show_all_history:
+    train_data[columns].plot(figsize=(16, 8), subplots=True)
+    plt.xlabel('Дата')
+    plt.show()
 
 for column in columns:
     settings = properties[column]
+    direct = settings[directory]
+    if settings[to_process]:
+        df = train_data[[column]]
+        forecast_out = int(math.ceil(settings[shift_percent] * len(df)))
 
-    df = train_data[[column]]
-    forecast_out = int(math.ceil(settings[shift_percent] * len(df)))
+        if settings[print_forecast_out]:
+            print(forecast_out)
 
-    if settings[print_forecast_out]:
-        print(forecast_out)
+        df = util.extend_shifted_data(df, column, forecast_out)
 
-    df = util.extend_shifted_data(df, column, forecast_out)
+        if settings[save_to_excel]:
+            util.save_to_excel(df, direct, 'shifted.xlsx')
 
-    if settings[need_show_sifted_figures]:
-        util.show_shifted_figure(df, column)
-    scaled_data = util.get_scaled_data(df)
+        if settings[need_show_sifted_figures]:
+            util.show_shifted_figure(df, column)
 
-    #  Выбор данных для прогнозирования
-    data_to_be_predicted = scaled_data[-forecast_out:]  # data to be predicted
-    data_to_be_trained = scaled_data[:-forecast_out]    # data to be trained
+        scaled_data = util.get_scaled_data(df)
 
-    # Получение целевых значений
-    df.dropna(inplace=True)
-    shifted = np.array(df['shifted'])
+        if settings[save_to_excel]:
+            util.save_to_excel(pd.DataFrame(scaled_data), direct, 'scaled_data.xlsx')
 
-    X_train, X_test, y_train, y_test = train_test_split(data_to_be_trained, shifted, test_size=0.2, random_state=42)
-    print('\n=============== Выбор модели для акций: ' + column + ' ===============\n')
-    model = util.get_model_with_high_accuracy(X_train, X_test, y_train, y_test)
+        #  Выбор данных для прогнозирования
+        '''
+            data_to_be_predicted: копия последних данных из scaled_data в количестве равному forecast_out
+            data_to_be_trained: копия данных из scaled_data за исключением последних в количестве равному forecast_out
+        '''
+        data_to_be_predicted = scaled_data[-forecast_out:]
+        data_to_be_trained = scaled_data[:-forecast_out]
 
-    last_date = df.index[-1]  # getting the lastdate in the dataset
-    last_unix = time.mktime(
-        datetime.datetime.strptime(last_date, "%Y.%m.%d").timetuple())  # converting it to time in seconds
-    one_day = 86400  # one day equals 86400 seconds
-    next_unix = last_unix + one_day  # getting the time in seconds for the next day
-    forecast_set = model.predict(data_to_be_predicted)  # predicting forecast data
-    df['Forecast'] = np.nan
-    for i in forecast_set:
-        next_date = datetime.datetime.fromtimestamp(next_unix)
-        next_unix += 86400
-        df.loc[next_date] = [np.nan for _ in range(len(df.columns) - 1)] + [i]
+        if settings[save_to_excel]:
+            util.save_to_excel(pd.DataFrame(data_to_be_predicted), direct, 'data_to_be_predicted.xlsx')
+            util.save_to_excel(pd.DataFrame(data_to_be_trained), direct, 'data_to_be_trained.xlsx')
 
-    plt.figure(figsize=(18, 8))
-    df[column].plot()
-    df['Forecast'].plot()
-    plt.legend(loc=4)
-    plt.xlabel('Date')
-    plt.ylabel('Price')
-    plt.show()
+        # Получение целевых значений
+        df.dropna(inplace=True)  # Remove missing values.
+        shifted = np.array(df['shifted'])
 
+        X_train, X_test, y_train, y_test = train_test_split(
+            data_to_be_trained,
+            shifted,
+            test_size=settings[test_size],
+            random_state=settings[random_state]
+        )
 
+        model = util.get_model_with_high_accuracy(direct, X_train, X_test, y_train, y_test)
 
+        last_date = df.index[-1]
+        last_unix = datetime.datetime.strptime(last_date, "%Y.%m.%d").timestamp()
+        one_day = 86400
+        next_unix = last_unix + one_day
+        forecast_set = model.predict(data_to_be_predicted)  # predicting forecast data
+        df['Forecast'] = np.nan
+        for i in forecast_set:
+            next_date = datetime.datetime.fromtimestamp(next_unix)
+            next_unix += one_day
+            df.loc[next_date] = [np.nan for _ in range(len(df.columns) - 1)] + [i]
+
+        df.plot(y=[column, 'Forecast'], figsize=(16, 8))
+        plt.legend(loc=4)
+        plt.xlabel('Date')
+        plt.ylabel('Price')
+        plt.show()
--- util.py
+++ util.py
@@ -1,4 +1,6 @@
+import os
 import numpy as np
+import pandas as pd
 from matplotlib import pyplot as plt
 from pandas import DataFrame
 from sklearn.ensemble import RandomForestRegressor
@@ -14,23 +16,27 @@ def extend_shifted_data(df: DataFrame, column: str, forecast_out: int):
 
 def show_shifted_figure(df: DataFrame, column: str):
     df.plot(y=['shifted', column], figsize=(16, 8))
-    plt.title('Курс акций: ' + column)  # adding a title
-    plt.xlabel('Дата')  # x label
-    plt.ylabel('Стоимость акции')  # y label
+    plt.title('Курс акций: ' + column)
+    plt.xlabel('Дата')
+    plt.ylabel('Стоимость акции')
     plt.show()
 
 
-# Масштабирование данных
-# В машинном обучении StandardScaler (стандартный масштабатор) используется для изменения
-# размера распределения значений так, чтобы среднее значение наблюдаемых значений было равно 0, а стандартное
-# отклонение – 1
+'''
+Масштабирование данных
+В машинном обучении StandardScaler (стандартный масштабатор) используется для изменения
+размера распределения значений так, чтобы среднее значение наблюдаемых значений было равно 0, а стандартное
+отклонение – 1
+'''
+
+
 def get_scaled_data(df: DataFrame):
     scaler = StandardScaler()
     X = np.array(df.drop(['shifted'], axis=1))
     return scaler.fit_transform(X)
 
 
-def get_model_with_high_accuracy(X_train, X_test, y_train, y_test):
+def get_model_with_high_accuracy(directory: str, X_train, X_test, y_train, y_test):
     models = {
         'Linear Regression': LinearRegression(),
         'Random Forest': RandomForestRegressor(),
@@ -40,16 +46,30 @@ def get_model_with_high_accuracy(X_train, X_test, y_train, y_test):
 
     score = .0
     method = ''
-    for mtd in models:
-        sc = get_score(models[mtd], X_train, X_test, y_train, y_test)
-        print(mtd + ' score: ' + str(sc))
+    idx = 0
+    if os.path.exists(directory + 'models_score.xlsx'):
+        df = pd.read_excel(directory + 'models_score.xlsx')
+        idx = df.index[-1] + 1
+    else:
+        df = pd.DataFrame(columns=['Linear Regression', 'Random Forest', 'Ridge', 'SVR'])
+
+    for column in df.columns.values:
+        sc = get_score(models[column], X_train, X_test, y_train, y_test)
+        df.loc[idx, column] = sc
         if sc > score:
             score = sc
-            method = mtd
-    print('\nВыбрана наилучшая модель: ' + method + '\nС точностью: ' + str(score))
+            method = column
+
+    save_to_excel(df, directory, 'models_score.xlsx')
     return models[method]
 
 
 def get_score(model, X_train, X_test, y_train, y_test):
     model.fit(X_train, y_train)
     return model.score(X_test, y_test)
+
+
+def save_to_excel(df: DataFrame, directory: str, file_name: str):
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    df.to_excel(directory + file_name, index=False)
