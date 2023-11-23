2de375372c5c5c72f16753f2ac6d0023dabeba89
izotov_a <aleksandr.izotov2020@gmail.com>
2023-11-23 21:44:04 +0300

    Поигрался с настройками для получения минимальной точности модели от 0.8

--- Settings.py
+++ Settings.py
@@ -11,12 +11,12 @@ show_all_history = False
 
 settings = {
     'VTB': {
-        to_process: True,
+        to_process: False,
         save_to_excel: True,
         directory: 'втб/',
         need_show_sifted_figures: False,
-        print_forecast_out: False,
-        shift_percent: .05,
+        print_forecast_out: True,
+        shift_percent: .01,
         test_size: .2,
         random_state: 42
     },
@@ -25,69 +25,69 @@ settings = {
         save_to_excel: True,
         directory: 'доллар/',
         need_show_sifted_figures: False,
-        print_forecast_out: False,
-        shift_percent: .05,
+        print_forecast_out: True,
+        shift_percent: .01,
         test_size: .2,
-        random_state: 42
+        random_state: 50
     },
     'Brent': {
         to_process: False,
         save_to_excel: True,
         directory: 'brent/',
         need_show_sifted_figures: False,
-        print_forecast_out: False,
-        shift_percent: .05,
+        print_forecast_out: True,
+        shift_percent: .01,
         test_size: .2,
-        random_state: 42
+        random_state: 43
     },
     'Gazprom': {
         to_process: False,
         save_to_excel: True,
         directory: 'газпром/',
         need_show_sifted_figures: False,
-        print_forecast_out: False,
-        shift_percent: .05,
+        print_forecast_out: True,
+        shift_percent: .01,
         test_size: .2,
-        random_state: 42
+        random_state: 41
     },
     'Лукойл': {
         to_process: False,
         save_to_excel: True,
         directory: 'лукойл/',
         need_show_sifted_figures: False,
-        print_forecast_out: False,
-        shift_percent: .05,
+        print_forecast_out: True,
+        shift_percent: .01,
         test_size: .2,
-        random_state: 42
+        random_state: 40
     },
     'Роснефть': {
         to_process: False,
         save_to_excel: True,
         directory: 'роснефть/',
         need_show_sifted_figures: False,
-        print_forecast_out: False,
-        shift_percent: .05,
+        print_forecast_out: True,
+        shift_percent: .01,
         test_size: .2,
-        random_state: 42
+        random_state: 41
     },
     'Сбербанк': {
         to_process: False,
         save_to_excel: True,
         directory: 'сбербанк/',
         need_show_sifted_figures: False,
-        print_forecast_out: False,
-        shift_percent: .05,
+        print_forecast_out: True,
+        shift_percent: .01,
         test_size: .2,
         random_state: 42
     },
     'Сургутнефтегаз': {
-        to_process: False,
+        to_process: True,
         save_to_excel: True,
         directory: 'сургутнефтегаз/',
         need_show_sifted_figures: False,
-        print_forecast_out: False,
-        shift_percent: .05,
+        print_forecast_out: True,
+        shift_percent: .01,
         test_size: .2,
-        random_state: 42
+        random_state: 44
     }
 }
